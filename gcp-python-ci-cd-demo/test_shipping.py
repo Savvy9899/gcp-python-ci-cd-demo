9607c1933a890b78ca4d0e0a502ed76dd9c78f0d
Senadeera S.A.P.R. <82633809+Savvy9899@users.noreply.github.com>
2026-05-14 20:00:55 +1000

    Changes to test file

--- gcp-python-ci-cd-demo/test_shipping.py
+++ gcp-python-ci-cd-demo/test_shipping.py
@@ -5,8 +5,8 @@ def test_standard_shipping_for_weight_five_or_less():
     assert calculate_shipping_category(5) == "STANDARD"
 
 def test_heavy_shipping_for_weight_above_five():
-    assert calculate_shipping_category(6) == "STANDARD"
-    # assert calculate_shipping_category(6) == "HEAVY"
+    assert calculate_shipping_category(6) == "HEAVY"
+    #assert calculate_shipping_category(6) == "STANDARD"
 
 def test_invalid_shipping_for_zero_or_negative_weight():
     assert calculate_shipping_category(0) == "INVALID"
